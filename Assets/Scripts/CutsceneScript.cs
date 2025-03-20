3be70d704823f530fbe335d976e71582922600e8
Onion155 <129494016+Onion155@users.noreply.github.com>
2025-03-20 16:42:20 +0000

    beep-boob-bap

--- Assets/Scripts/CutsceneScript.cs
+++ Assets/Scripts/CutsceneScript.cs
@@ -2,24 +2,31 @@ using UnityEngine;
 
 public class CutsceneScript : MonoBehaviour
 {
-    public GameObject camera1;
-    public GameObject camera2;
+    public GameObject MainCamera; // main camera
+    public GameObject CutsceneCamera; // cut scene camera
     public Animator animator1;
 
-    public void Start()
-    {
-    }
+ 
     public void CutsceneStart()
     {
-        camera1.SetActive(false);
-        camera2.SetActive(true);
+        MainCamera.SetActive(false);
+        CutsceneCamera.SetActive(true);
         animator1.SetBool("playanim", true);
         Debug.Log("playanim set true");
+        Invoke("CutsceneEnd", 4f);
     }
 
     public void CutsceneEnd()
     {
-        camera1.SetActive(true);
-        camera2.SetActive(false);
+        MainCamera.SetActive(true);
+        CutsceneCamera.SetActive(false);
+    }
+    public void SetCusceneCamera(GameObject camera)
+    {
+        CutsceneCamera = camera;
+    }
+    public void SetAnimator(Animator animator)
+    {
+        animator1 = animator;
     }
 }
